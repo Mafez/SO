3f8b1dacd9ca007e4275c2423dc6e1420f5a4ec7
Manuel Antonio <manfer05@ucm.es>
2017-11-07 17:20:25 +0100

    2/3 de la avanzada

---
+++ Practica1/mytar.h
@@ -0,0 +1,24 @@
+#ifndef _MYTAR_H
+#define _MYTAR_H
+
+#include <limits.h>
+#include <stdint.h>
+
+typedef enum{
+  NONE,
+  ERROR,
+  CREATE,
+  EXTRACT
+} flags;
+
+typedef struct {
+  char* name;
+  unsigned int size;
+  uint16_t crc;
+} stHeaderEntry;
+
+int createTar(int nFiles, char *fileNames[], char tarName[]);
+int extractTar(char tarName[]);
+
+
+#endif /* _MYTAR_H */
